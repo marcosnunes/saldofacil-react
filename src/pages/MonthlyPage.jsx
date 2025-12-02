ee99aa43f69ed217683d51dd3677000761417eb7
marcosnunes <marcos.nunes.lph@outlook.com>
2025-12-01 22:56:43 -0300

    Update

--- src/pages/MonthlyPage.jsx
+++ src/pages/MonthlyPage.jsx
@@ -19,7 +19,10 @@ export default function MonthlyPage() {
 
 function MonthlyContent({ monthIndex }) {
   const { initialBalance, tithe, creditCardBalance, investmentBalance, totalCredit, totalDebit, balance, finalBalance, percentage, addTransaction, updateTransaction, deleteTransaction, importOFX, setInitialBalance } = useMonthly();
-  const { selectedYear } = useMonthly();
+  let { selectedYear } = useMonthly();
+  if (!selectedYear) {
+    selectedYear = new Date().getFullYear();
+  }
   const monthName = monthsPT[monthIndex];
   const navigate = useNavigate();
   const prevMonth = monthIndex === 0 ? 12 : monthIndex;
