a273b6026d31cf218c85529304f07b62c3138a7f
marcosnunes <marcos.nunes.lph@outlook.com>
2026-06-17 22:25:45 -0300

    Update

--- electron-builder.cjs
+++ electron-builder.cjs
@@ -6,6 +6,7 @@ const displayName = process.env.MS_STORE_DISPLAY_NAME || 'Saldo Fácil';
 module.exports = {
   appId: 'com.autossuficiencia.saldofacil',
   productName: 'SaldoFacil',
+  artifactName: '${productName}-${version}-${os}-${arch}.${ext}',
   directories: {
     output: 'release',
   },
@@ -30,5 +31,14 @@ module.exports = {
     displayName,
     backgroundColor: '#5e72e4',
     setBuildNumber: true,
+  },
+  nsis: {
+    oneClick: false,
+    allowToChangeInstallationDirectory: true,
+    perMachine: false,
+    allowElevation: true,
+    createDesktopShortcut: true,
+    createStartMenuShortcut: true,
+    shortcutName: 'SaldoFacil',
   },
 };
